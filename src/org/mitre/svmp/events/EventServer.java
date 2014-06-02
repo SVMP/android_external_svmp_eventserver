10999f5b6bc526cb79d93671c1a7dd77dfed3337
David Keppler <dkeppler@mitre.org>
2014-06-02 14:13:29 -0400

    Eliminate network jitter from touch input replay
    Previously, the touch handler used message receive timestamps when
    re-injecting touch events into the Android framework. This gave ample
    opportunity for network jitter to mangle the gesture playback, introducing
    skips, jumps, and other undesired behavior.
    
    This patch uses client-side event timing offset to account for the
    difference in uptimeMillis() between the client and VM. When a new DOWN
    event is received, the offset between the two uptime counters is calculated
    and stored. Then, the event times of the following stream of pointer MOVEs
    are adjusted according to this offset.
    
    Also added is processing of "Historical" batched MotionEvents where a
    series of pointer movements are batched up into a single Event. Ignoring
    these, if the client actually produces them, would be another source of
    skipping and jumping in gesture playback.
    
    Finally, also added support for the "edgeFlags" field just in case anything
    uses those flags for things like "swipe in from edge" gestures. Discussion
    online indicate these flags aren't typically used, but just in case...
    
    Change-Id: Ib479e7e46428e5c91c1b65cd05fd06ed4102c299

--- src/org/mitre/svmp/events/EventServer.java
+++ src/org/mitre/svmp/events/EventServer.java
@@ -43,7 +43,7 @@ public class EventServer extends BaseServer {
 
     private IWindowManager windowManager;
     private Display display;
-    private long lastDownTime;
+    private long lastDownTime, lastDownTimeClient = -1;
     private final Point screenSize = new Point();
     private double xScaleFactor, yScaleFactor;
 
@@ -51,10 +51,10 @@ public class EventServer extends BaseServer {
         super(context);
         windowManager = IWindowManager.Stub.asInterface(ServiceManager.getService("window"));
         display = DisplayManagerGlobal.getInstance().getRealDisplay(Display.DEFAULT_DISPLAY);
- 
+
         // try{
-          // windowManager.getRealDisplaySize(screenSize);	  
-          display.getRealSize(screenSize);
+        // windowManager.getRealDisplaySize(screenSize);
+        display.getRealSize(screenSize);
         // } catch (RemoteException re){
         //   Utility.logError("Error getting display size: " + re.getMessage());
         // }
@@ -77,6 +77,52 @@ public class EventServer extends BaseServer {
         Log.d(TAG, "Sent screen info response: " + screenSize.x + "," + screenSize.y);
     }
 
+    private MotionEvent.PointerCoords translateCoords(float X, float Y) {
+        MotionEvent.PointerCoords coords = new MotionEvent.PointerCoords();
+
+        // Translate the client coordinates according to the screen orientation
+        // Origin is the top left of the physical phone's screen in the natural upright position
+        int rotation = Surface.ROTATION_0;
+        try {
+            rotation = windowManager.getRotation();
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    coords.x = X;
+                    coords.y = Y;
+                    break;
+                case Surface.ROTATION_180:
+                    // screen turned left 180
+                    // client origin is now in bottom right
+                    // invert both
+                    coords.x = screenSize.x - X;
+                    coords.y = screenSize.y - Y;
+                    break;
+                case Surface.ROTATION_90:
+                    // screen turned left 90
+                    // client origin is now in bottom left
+                    // switch, invert client x
+                    coords.x = Y;
+                    coords.y = screenSize.x - X;
+                    break;
+                case Surface.ROTATION_270:
+                    // screen turned right 90
+                    // client origin is now in top right
+                    // switch, invert client y
+                    coords.x = screenSize.y - Y;
+                    coords.y = X;
+                    break;
+            }
+        } catch (RemoteException re) {
+            Log.e(TAG, "Cannot translate input coordinates. Error getting display size: " + re.getMessage());
+            coords.x = X;
+            coords.y = Y;
+        } finally {
+            coords.pressure = 1f;
+            coords.size = 5f;
+        }
+        return coords;
+    }
+
     @Override
     public void handleTouch(final List<TouchEvent> eventList) {
         // we can receive a batch of touch events; process each event individually
@@ -86,73 +132,136 @@ public class EventServer extends BaseServer {
 
     // overload to handle individual touch events
     public void handleTouch(final SVMPProtocol.TouchEvent event) {
+        if (event.hasEventTime())
+            handleTouchNew(event);
+        else
+            handleTouchOld(event);
+    }
+
+    private final void handleTouchOld(final SVMPProtocol.TouchEvent event) {
         // Maintain Downtime
         final long now = SystemClock.uptimeMillis();
         if (event.getAction()== MotionEvent.ACTION_DOWN ||
                 event.getAction() == MotionEvent.ACTION_POINTER_1_DOWN ||
                 event.getAction() == MotionEvent.ACTION_POINTER_2_DOWN)
+        {
             lastDownTime = now;
+        }
+
         // Create the MotionEvent to inject
         final int pointerSize = event.getItemsCount();
         MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerSize];
         MotionEvent.PointerProperties[] props = new MotionEvent.PointerProperties[pointerSize];
+
         for (int i = 0; i < pointerSize; i++) {
             props[i] = new MotionEvent.PointerProperties();
-            coords[i] = new MotionEvent.PointerCoords();
             props[i].id = event.getItems(i).getId();
-            props[i].toolType = 0;
-            // Added testing scale - Problem with calculating scale correctly - goes to 0
-        //final float newX = event.pointerItems.get(i).x * 0.4f;
-            // origin is the top left of the physical phone's screen in portrait
-            int rotation = Surface.ROTATION_0;
-            try{
-                rotation = windowManager.getRotation();
-            } catch (RemoteException re){
-                Log.e(TAG, "Error getting display size: " + re.getMessage());
-            }
-            switch (rotation) {
-                case Surface.ROTATION_0:
-                    coords[i].x = event.getItems(i).getX();
-                    coords[i].y = event.getItems(i).getY();
-                    break;
-                case Surface.ROTATION_180:
-                	// screen turned left 180
-                	// client origin is now in bottom right
-                	// invert both
-                	coords[i].x = screenSize.x - event.getItems(i).getX();
-                	coords[i].y = screenSize.y - event.getItems(i).getY();
-                    break;
-                case Surface.ROTATION_90:
-                	// screen turned left 90
-                	// client origin is now in bottom left
-                	// switch, invert client x
-                    coords[i].x = event.getItems(i).getY();
-                    coords[i].y = screenSize.x - event.getItems(i).getX();
-                    break;
-                case Surface.ROTATION_270:
-                	// screen turned right 90
-                	// client origin is now in top right
-                	// switch, invert client y
-                	coords[i].x = screenSize.y - event.getItems(i).getY();
-                	coords[i].y = event.getItems(i).getX();                       	
-                    break;
-            }
-            coords[i].pressure = 1f;
-            coords[i].size = 5f;
+            props[i].toolType = MotionEvent.TOOL_TYPE_FINGER;
+            coords[i] = translateCoords(event.getItems(i).getX(), event.getItems(i).getY());
         }
 
         MotionEvent me = MotionEvent.obtain(lastDownTime, now, event.getAction(), pointerSize, props, coords,
-                                            0, 0, 1, 1, 0, 0, InputDevice.SOURCE_TOUCHSCREEN, 0);
-       try {
-    	  //Utility.logInfo("injecting touch event");
-          // windowManager.injectPointerEvent(me,false);
-          InputManager.getInstance().injectInputEvent(me,InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT); 
-       } catch (Exception e) {
-         // ignore for now 
-       } finally {
-           me.recycle();
-       }
+                0, 0, 1, 1, 0, 0, InputDevice.SOURCE_TOUCHSCREEN, 0);
 
+        injectTouch(me);
+    }
+
+    private long lastTouch = 0;
+    private void handleTouchNew(final SVMPProtocol.TouchEvent event) {
+        // Maintain Downtime
+        final long now = SystemClock.uptimeMillis();
+        long eventTime = event.getEventTime();
+
+        if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
+            Log.d(TAG, "ACTION_DOWN( " + event.getItems(0).getId() + ") at " + now + " / " + eventTime);
+            lastDownTime = now;
+            lastDownTimeClient = event.getDownTime();
+        }
+
+        if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
+            Log.d(TAG, "ACTION_UP( " + event.getItems(0).getId() + ") at " + now + " / " + eventTime);
+            // give UP's a little bit of leeway in the timing 
+            // makes them less likely to get dropped by the system and reduces touch input weirdness
+            eventTime += 75;
+        }
+
+        if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_POINTER_UP) {
+            int index = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+            Log.d(TAG, "ACTION_POINTER_UP(" + event.getItems(index).getId() + ") at " + now + " / " + eventTime);
+        }
+
+        if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_POINTER_DOWN) {
+            int index = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+            Log.d(TAG, "ACTION_POINTER_DOWN(" + event.getItems(index).getId() + ") at " + now + " / " + eventTime);
+        }
+
+        // translate the pointer coordinates based on screen orientation
+        final int pointerSize = event.getItemsCount();
+        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerSize];
+        MotionEvent.PointerProperties[] props = new MotionEvent.PointerProperties[pointerSize];
+
+        for (int i = 0; i < pointerSize; i++) {
+            props[i] = new MotionEvent.PointerProperties();
+            props[i].id = event.getItems(i).getId();
+            props[i].toolType = MotionEvent.TOOL_TYPE_FINGER;
+            coords[i] = translateCoords(event.getItems(i).getX(), event.getItems(i).getY());
+        }
+
+        // use client edgeFlags if present
+        int edgeFlags = event.hasEdgeFlags() ? event.getEdgeFlags() : 0;
+
+        MotionEvent me = MotionEvent.obtain(
+                lastDownTime,                           // downTime
+                offsetEventTime(eventTime),             // eventTime
+                event.getAction(),                      // action
+                pointerSize,                            // pointerCount
+                props,                                  // pointerProperties
+                coords,                                 // pointerCoords
+                0,                                      // metaState
+                0,                                      // buttonState
+                1,                                      // xPrecision
+                1,                                      // yPrecision
+                0,                                      // deviceId
+                edgeFlags,                              // edgeFlags
+                InputDevice.SOURCE_TOUCHSCREEN,         // source
+                0 );                                    // flags
+
+        // handle any batched historical pointer movements
+        int historicalSize = event.getHistoricalCount();
+        for (int i = 0; i < historicalSize; i++) {
+            SVMPProtocol.TouchEvent.HistoricalEvent h = event.getHistorical(i);
+
+            // translate the coordinate space to local screen orientation
+            int coordsSize = event.getHistorical(i).getCoordsCount();
+            coords = new MotionEvent.PointerCoords[coordsSize];
+            for (int j = 0; j < coordsSize; j++) {
+                coords[j] = translateCoords(h.getCoords(j).getX(), h.getCoords(j).getY());
+            }
+
+            me.addBatch(offsetEventTime(h.getEventTime()), coords, 0);
+        }
+
+        me.setAction(event.getAction());
+
+        injectTouch(me);
+    }
+
+    private long offsetEventTime(long clientTime) {
+        return clientTime - lastDownTimeClient + lastDownTime;
+    }
+
+    private void injectTouch(MotionEvent me) {
+        try {
+            //Log.d(TAG, "injecting touch event");
+            //InputManager.getInstance().injectInputEvent(me,InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT);
+            if (!InputManager.getInstance().injectInputEvent(me,InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH))
+                Log.e(TAG, "Failed injecting MotionEvent " + me.toString());
+        } catch (Exception e) {
+            Log.e(TAG, "Error injecting MotionEvent: " + e.getMessage());
+        } finally {
+            me.recycle();
+        }
     }
 
 }
+
